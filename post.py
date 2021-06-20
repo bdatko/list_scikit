ec13cc4637b8c9cd82abc5c899ce5204e8d13641
benda <>
2021-06-19 22:57:47 -0400

    feat(post.py): add cli for craete the venn diagram

--- post.py
+++ post.py
@@ -6,9 +6,16 @@ from pathlib import Path
 from typing import Union
 
 import fire
+import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib_venn import venn2
 
 from list_scikit import MY_LIST_DICT
+
+FONT = {"family": "serif", "size": 22}
+
+matplotlib.rc("font", **FONT)
 
 
 def create_scikit_offical(source_data: Union[Path, str], output_file: Union[Path, str]):
@@ -28,7 +35,18 @@ def create_scikit_offical(source_data: Union[Path, str], output_file: Union[Path
 
 def create_venn_figure(source_data: Union[Path, str], output_file: Union[Path, str]):
     libraries = pd.read_csv(source_data)
-    pass
+    fig, ax = plt.subplots(figsize=(12, 12))
+    v = venn2(
+        [set(libraries["library"].to_list()), set(MY_LIST_DICT.keys())],
+        ("Scikit-learn Related Project Official", "My List"),
+        ax=ax,
+    )
+    fig.savefig(
+        output_file,
+        bbox_inches="tight",
+        facecolor=fig.get_facecolor(),
+        dpi=600,
+    )
 
 
 def create_post(source_data: Union[Path, str], output_file: Union[Path, str]):
@@ -39,5 +57,6 @@ if __name__ == "__main__":
     fire.Fire(
         {
             "create_scikit_offical": create_scikit_offical,
+            "create_venn_figure": create_venn_figure,
         }
     )
